5aebee1d79c692e70ef678a598e0d5b46d75cb45
vc <vince18546@hotmail.com>
2014-08-05 14:50:17 +0800

    Add Prototype
    It can find the gadgets
    * pop ebp; ret
    * mov xxx; ret

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "rop.h"
+
+int main(void)
+{
+        FILE *fp;
+        char file_name[20];
+	char *binary;
+        unsigned long binary_len;
+
+        printf("Enter binary file name: ");
+        scanf("%s",file_name);
+        fp = fopen(file_name, "rb");
+
+        //Get file length
+        fseek(fp, 0, SEEK_END);
+        binary_len=ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        //Allocate memory
+        binary = (char *)malloc(binary_len+1);
+        if(fp){
+                fread(binary,binary_len,1,fp);
+        }   
+        fclose(fp);
+
+	rop_findgadgets(binary, binary_len);
+	free(binary);
+	return 0;
+}
---
+++ rop.c
@@ -0,0 +1,50 @@
+#include "rop.h"
+
+int rop_findgadgets(char *binary, unsigned long binary_len)
+{
+
+	size_t count;
+	csh handle;
+	cs_insn *insn;
+
+	struct Gadget GADGET;
+	strcpy(GADGET.string,"");
+
+	if (cs_open(CS_ARCH_X86, CS_MODE_32, &handle) != CS_ERR_OK)
+		return -1;
+	count = cs_disasm_ex(handle, binary, binary_len, 0x08048000, 0, &insn);
+	if (count > 0) {
+		rop_find("pop","ebp",count,insn,GADGET);
+		rop_find("mov","xxx",count,insn,GADGET);
+		cs_free(insn, count);
+	} 
+	else{
+		printf("ERROR: Failed to disassemble given code!\n");
+	}
+	cs_close(&handle);
+	return 0;
+}
+
+int rop_find(char* operate, char* operand, size_t count, cs_insn *insn, struct Gadget GADGET)
+{
+	size_t j;
+	for (j = 0; j < count; j++) {
+		//printf("0x0%"PRIx64":\t%s\t\t%s\n", insn[j].address, \
+			insn[j].mnemonic,insn[j].op_str);
+		if(!strcmp(insn[j].mnemonic,"ret") && !strcmp(insn[j-1].mnemonic,operate) && \
+			(!strcmp(insn[j-1].op_str,operand) || !strcmp(operand,"xxx")))
+		{
+			strcat(GADGET.string,insn[j-1].mnemonic);
+			strcat(GADGET.string," ");
+			strcat(GADGET.string,insn[j-1].op_str);
+			strcat(GADGET.string," ; ");
+			GADGET.address = insn[j-1].address;
+			strcat(GADGET.string,"ret");
+			printf("0x0%x: %s\n",GADGET.address,GADGET.string);
+			strcpy(GADGET.string,"");
+			return 0;		
+		}
+	}
+	printf("Can't find '%s %s; ret'\n",operate,operand);
+	return -1;
+}
