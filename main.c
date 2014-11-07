d92e602883c8eba3d6e92e83c02d02f083ae56d6
vc <vince18546@hotmail.com>
2014-11-07 15:18:54 +0800

    Pasring arg [-o offset]: Add padding bytes; Check odd argc error

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 void usage(void)
 {
     printf("Usage:\n");
-    printf(" ropchain <file> [-p true|false]\n");
+    printf(" ropchain <file> [-p <bool>] [-o <offset>]\n");
     printf(" -p\tPrint all gadgets\n");
+    printf(" -o\tAdd padding(offset) bytes\n");
     exit (-1);
 }
 
@@ -16,6 +17,8 @@ int main(int argc, char** argv)
     unsigned char *chain;
     int chain_len = 0;
     int i;
+    int offset = 0;
+    char *endptr;
     bool arg_print = 1;
 
     if(argc < 2)
@@ -52,24 +55,37 @@ int main(int argc, char** argv)
     //Parse command
     while ((argc > 1) && (argv[1][0] == '-'))
     {
-        switch (argv[1][1])
+        if(argv[2])
         {
-            case 'p':
-                if(!strcmp(argv[2], "false") || !strcmp(argv[2], "0"))
-                {
-                    arg_print = 0;
-                }
-                break;
-            default:
-                usage();
+            switch (argv[1][1])
+            {
+                case 'p':
+                    if(!strcmp(argv[2], "false") || !strcmp(argv[2], "0"))
+                    {
+                        arg_print = 0;
+                    }
+                    break;
+                case 'o':
+                    offset = strtol(argv[2], &endptr, 10);
+                    break;
+                default:
+                    usage();
+            }
+            argv += 2;
+            argc -= 2;
         }
-        argv += 2;
-        argc -= 2;
+        else
+        {
+            fprintf(stderr, "Parser args failed.[%s]\n", argv[1]);
+            return -1;
+        }
     }
     chain_len = rop_chain(&chain, binary, binary_len, arg_print);
     if(chain_len > 0)
     {
         printf("\n--- Result ---\n");
+        for(i = 0; i < offset; i++)
+            printf("\\x41");
         for(i = 0; i < chain_len; i++)
             printf("\\x%02x",chain[i]);
         printf("\n");
